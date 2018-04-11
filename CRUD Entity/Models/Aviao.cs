eb58b9e9f3710267d0f702fe137c2f7e2cb22180
ferjesusjs8 <fernando.js8@outlook.com>
2018-04-11 12:32:16 -0300

    Falta salvar aviao

--- CRUD Entity/Models/Aviao.cs
+++ CRUD Entity/Models/Aviao.cs
@@ -10,11 +10,20 @@ namespace CRUD_Entity.Models
     public class Aviao
     {
         [Key]
+        [Required]
         public int IdAviao { get; set; }
         [ForeignKey("Pilotos")]
+        [Display(Name = "Nome do Piloto")]
+        [Required]
         public int PilotoRefId { get; set; }
+        [Required]
+        [Display(Name = "Nome do Piloto")]
         public Piloto Pilotos { get; set; }
+        [Required]
+        [Display(Name = "Modelo")]
         public string Modelo { get; set; }
+        [Required]
+        [Display(Name = "Marca")]
         public string Marca { get; set; }
     }
 }
--- CRUD Entity/Models/Piloto.cs
+++ CRUD Entity/Models/Piloto.cs
@@ -9,11 +9,22 @@ namespace CRUD_Entity.Models
     public class Piloto
     {
         [Key]
+        [Required]
         public int IdPiloto { get; set; }
+        [Required]
+        [Display(Name = "RG")]
         public int RG { get; set; }
+        [Required]
+        [Display(Name = "Número da Licença")]
         public int NumeroLicenca { get; set; }
+        [Required]
+        [Display(Name ="CPF / CNPJ")]
         public long CPFCNPJ { get; set; }
+        [Required]
+        [Display(Name = "Nome")]
         public string Nome { get; set; }
+        [Required]
+        [Display(Name = "Sobrenome")]
         public string Sobrenome { get; set; }
 
         public ICollection<Aviao> Avioes { get; set; }
